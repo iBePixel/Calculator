c7412cd460b2aa58eface3fe44363b326e78a09f
Elliott Lamb <53214529+iBePixel@users.noreply.github.com>
2024-11-04 19:29:00 +0000

    Operator functions

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    let result = num1 + num2;
+    return result
+}
+
+function subtract(num1, num2) {
+    let result = num1 - num2;
+    return result
+}
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result
+}
+
+function divide(num1, num2) {
+    let result = num1 / num2;
+    return result
+}
