3ef90a15d5dc05174aa8c4b336ad779ddddc4c36
Elliott Lamb <53214529+iBePixel@users.noreply.github.com>
2024-11-05 18:26:56 +0000

    Tidy up numListener function

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 const screen = document.querySelector("#screen");
-const pad = document.querySelectorAll("#numPad")
+const digits = document.querySelectorAll("#number");
+
 
 function add(num1, num2) {
     let result = num1 + num2;
@@ -21,23 +22,23 @@ function divide(num1, num2) {
     return result
 }
 
-let firstNum = '1'
-let secondNum
+let firstNum = ''
+let secondNum = ''
 let operator
 
 function updateScreen(number) {
     screen.textContent = number;
 }
 
-function addListeners(){
-    for (let i = 0; i < pad.length; i++){
-        let key = pad[i].textContent
+function addNumListeners(){
+    for (let i = 0; i < digits.length; i++){
+        let key = digits[i].textContent
         key = parseInt(key)
-        if (key != NaN) {
-            pad[i].addEventListener("click", () => {
-                firstNum = firstNum.concat(key)
-                updateScreen(firstNum);
-            })
-        }
+        digits[i].addEventListener("click", () => {
+            firstNum = firstNum.concat(key)
+            updateScreen(firstNum);
+        })
     }
 }
+
+addNumListeners()
