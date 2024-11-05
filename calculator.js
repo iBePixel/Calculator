fd0ae83db864660e9f3c27fd5f778a57a82b954e
iBePixel <53214529+iBePixel@users.noreply.github.com>
2024-11-05 16:29:00 +0000

    Modifying function to add listeners for numeric buttons. Updated id's so non numbered buttons are excluded

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
 const screen = document.querySelector("#screen");
+const pad = document.querySelectorAll("#numPad")
 
 function add(num1, num2) {
     let result = num1 + num2;
@@ -20,10 +21,23 @@ function divide(num1, num2) {
     return result
 }
 
-let firstNum
+let firstNum = '1'
 let secondNum
 let operator
 
 function updateScreen(number) {
     screen.textContent = number;
+}
+
+function addListeners(){
+    for (let i = 0; i < pad.length; i++){
+        let key = pad[i].textContent
+        key = parseInt(key)
+        if (key != NaN) {
+            pad[i].addEventListener("click", () => {
+                firstNum = firstNum.concat(key)
+                updateScreen(firstNum);
+            })
+        }
+    }
 }
